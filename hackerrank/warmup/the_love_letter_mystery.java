4afa8ae41ec227b229ad12560a686c47196d0540
Jacob <jacob.meline@gmail.com>
2015-04-23 05:55:43 +0000

    Working on an algorithm to solve this

--- hackerrank/warmup/the_love_letter_mystery.java
+++ hackerrank/warmup/the_love_letter_mystery.java
@@ -19,23 +19,38 @@ class Solution {
         } 
         return true;
     }
+    public static char getBiggestCharacter(String value) {
+        int highestCharacter = 0;
+        for (int j = 0; j < len; ++j) { 
+            int ascii_value = (int)value.charAt(i); 
+            if (ascii_value > highestCharacter) {
+                highestCharacter = ascii_value;
+            }
+        }
+        return (char)highestCharacter;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int cases = in.nextInt();
 
+        int count;
         for (int i = 0; i < cases; ++i) {
             String value = in.nextLine();
 
+            count = 0;
+            // first check if the string is a palindrom
             if (isPalindrome(value)){
-                System.out.println(value + " is a palindrome");
+                // System.out.println(value + " is a palindrome");
+                System.out.println(count);
             } else
                 System.out.println(value + " isn't a palindrome");
                 int len = value.length();
 
-                for (int j = 0; j < len; ++j) { 
-
+                for ( int i = 0; i< len; ++i) {
+                    for ( int j = 0; j < len; ++j) {
+                        
+                    }
                 }
-                    // first check if the string is a palindrom
 
         }
         System.out.println("Got String: " + value);
