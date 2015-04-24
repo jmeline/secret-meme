42c5f8bd68c4ede03e6aa8f3f32ad8a5353b8c36
Jacob <jacob.meline@gmail.com>
2015-04-24 05:55:47 +0000

    Adding algorithm for love-letter

--- hackerrank/warmup/the_love_letter_mystery.java
+++ hackerrank/warmup/the_love_letter_mystery.java
@@ -19,15 +19,28 @@ class Solution {
         } 
         return true;
     }
-    public static char getBiggestCharacter(String value) {
+    public static int getBiggestCharacterIndex(String value) {
         int highestCharacter = 0;
         for (int j = 0; j < len; ++j) { 
             int ascii_value = (int)value.charAt(i); 
             if (ascii_value > highestCharacter) {
-                highestCharacter = ascii_value;
+                highestCharacter = j;
             }
         }
-        return (char)highestCharacter;
+        return highestCharacter;
+    }
+
+    // helper functions
+    public static int getCharacter(char c) {
+        return (int)c;
+    }
+    public static char convertCharacter(int i){
+        return (char)i;
+    }
+    public static String replace(String str, int index, char value){
+        char[] tmp = str.toCharArray();
+        tmp[index] = value;
+        return String.valueOf(tmp);
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -42,16 +55,31 @@ class Solution {
             if (isPalindrome(value)){
                 // System.out.println(value + " is a palindrome");
                 System.out.println(count);
-            } else
+            } else {
                 System.out.println(value + " isn't a palindrome");
                 int len = value.length();
 
-                for ( int i = 0; i< len; ++i) {
-                    for ( int j = 0; j < len; ++j) {
-                        
-                    }
-                }
+                // find the largest character
+                int index = getBiggestCharacterIndex(value);
+                while(index >= 97)
+                {
+                    /* reduce it */
 
+                    // convert index to a character
+                    char c = convertCharacter(index)
+                    // convert character to ascii value
+
+                    // reduce ascii value by one
+                    
+                    // convert reduced ascii value back to character
+                    // assign character to value[index]
+                    
+                    value[index] = value.charAt(index)--;
+                    
+                    // check if palindrome 
+                    // repeat
+                }
+            }
         }
         System.out.println("Got String: " + value);
 
