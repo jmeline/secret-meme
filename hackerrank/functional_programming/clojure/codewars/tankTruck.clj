e75f3c5c3573b886a1bd2546552a7d65f4d8fe6a
Jacob Meline <jacob.meline@gmail.com>
2015-09-15 14:24:26 -0700

    Starting Tank Truck codewars kata in clojure

---
+++ hackerrank/functional_programming/clojure/codewars/tankTruck.clj
@@ -0,0 +1,16 @@
+;; Tank Truck
+;;
+;; To introduce the problem think to my neighbor who drives a tanker truck. The level indicator is down and is worried because he does not know if he will be able to make deliveries. We put the truck on a horizontal ground and measured the height of liquid in the tank. Fortunately the tank is a perfect cylinder and its vertical walls are flat. The height of the remaining liquid is h, the diameter of the cylinder is d, the total volume is vt (h,d,vtare positive or null integers). You can assume that h <= d.
+;;
+;; Could you calculate the remaining volume? Your function tankvol(h, d, vt) returns an integer which is the truncated result (e.g floor) of your float calculation.
+;;
+;; Examples:
+;;
+;; tankvol(40,120,3500)) should return 1021 (calculation gives about: 1021.26992027)
+;;
+;; tankvol(60,120,3500)) should return 1750
+;;
+;; tankvol(80,120,3500)) should return 2478 (calculation gives about: 2478.73007973)
+
+(defn tank-vol [h d vt]
+  )
